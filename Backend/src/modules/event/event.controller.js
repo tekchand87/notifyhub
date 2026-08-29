3ffbf4d16ada40059877f5392cca67889485517a
tekchand87 <tekchandyadav45@gmail.com>
2026-08-29 13:39:10 +0530

    fix : fix error in the event

--- Backend/src/modules/event/event.controller.js
+++ Backend/src/modules/event/event.controller.js
@@ -2,49 +2,28 @@ import * as eventService
   from "./event.service.js";
 
 
-export const publishEvent = async (
-  req,
-  res,
-  next
-) => {
+export const publishEvent = async (req,res,next) => {
   try {
+    const result =await eventService.publishEvent(req.tenantId,req.body);
 
-    const result =
-      await eventService.publishEvent(
-        req.tenantId,
-        req.body
-      );
-
-    return res
-      .status(202)
-      .json({
+    return res.status(202).json({
         success: true,
         message: "Event accepted",
         data: result
       });
-
-  } catch (error) {
+  } 
+  catch (error) {
     next(error);
   }
 };
 
 
-export const listEvents = async (
-  req,
-  res,
-  next
-) => {
+export const listEvents = async (req,res,next) => {
   try {
-
     const result =
-      await eventService.listEvents(
-        req.user.tenantId,
-        req.query
-      );
+      await eventService.listEvents(req.user.tenantId,req.query);
 
-    return res
-      .status(200)
-      .json({
+    return res.status(200).json({
         success: true,
         data: result
       });
--- Backend/src/modules/event/event.routes.js
+++ Backend/src/modules/event/event.routes.js
@@ -5,12 +5,10 @@ import * as eventController from "./event.controller.js";
 import {createEventSchema} from "./event.validator.js";
 
 import { validate } from "../../middleware/validate.middleware.js";
-
 import {requireApiKey} from "../../middleware/apiKeyauth.middleware.js";
-
 import {requireApiKeyScope} from "../../middleware/requireApiKeyScope.middleware.js";
-
 import {requireAuth} from "../../middleware/auth.middleware.js";
+
 
 import {EVENT_WRITE_SCOPE} from "./event.constants.js";
 
@@ -37,11 +35,7 @@ router.post(
   Tenant admin
   List Events
 */
-router.get(
-  "/",
-  requireAuth,
-  eventController.listEvents
-);
+router.get("/",requireAuth,eventController.listEvents);
 
 
 /*
